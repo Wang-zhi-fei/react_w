525b5bd913e8947005f7446a49864b8fe94c4e70
wangzhifei <2434689362@qq.com>
2019-12-27 02:11:31 +0800

    搭建脚手架

---
+++ src/Setting/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Button } from 'antd';
+import './css/setting.css'
+
+export default class App extends Component {
+  render() {
+    return (
+        
+      <div id="wrap">
+        <div className="header">
+          
+          <Button type="primary">Button</Button>
+        </div>
+      </div>
+    )
+  }
+}
