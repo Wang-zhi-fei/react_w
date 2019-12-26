aa13ddd67394d96e68d6bb5628918a82ddb5ca71
wangzhifei <2434689362@qq.com>
2019-12-27 02:45:54 +0800

    搭建脚手架，添加Antd自定义组件

--- src/Setting/App.js
+++ src/Setting/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
-import { Button } from 'antd';
+import { Card } from 'antd';
 import './css/setting.css'
-
+const gridStyle = {
+  width: '25%',
+  textAlign: 'center',
+};
 export default class App extends Component {
   render() {
     return (
         
       <div id="wrap">
         <div className="header">
-          
-          <Button type="primary">Button</Button>
+          <Card className="details" title="Card Title">
+            <Card.Grid style={gridStyle}>Content</Card.Grid>
+            <Card.Grid hoverable={false} style={gridStyle}>
+              Content
+            </Card.Grid>
+            <Card.Grid style={gridStyle}>Content</Card.Grid>
+            <Card.Grid style={gridStyle}>Content</Card.Grid>
+            <Card.Grid style={gridStyle}>Content</Card.Grid>
+            <Card.Grid style={gridStyle}>Content</Card.Grid>
+            <Card.Grid style={gridStyle}>Content</Card.Grid>
+          </Card>
         </div>
       </div>
     )
